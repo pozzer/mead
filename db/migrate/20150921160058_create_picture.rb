6629ddc02c61f90c15928cb8c38b8e0d1d906a99
pozzer <feliphepozzer@gmail.com>
2015-09-21 13:05:40 -0300

    table picture

---
+++ db/migrate/20150921160058_create_picture.rb
@@ -0,0 +1,14 @@
+class CreatePicture < ActiveRecord::Migration
+  def change
+    create_table :pictures do |t|
+      t.integer :user_id
+      t.string :picture_file_name
+      t.string :picture_content_type
+      t.integer :picture_file_size
+      t.datetime :picture_update_at
+      t.integer :attachable_id
+      t.string :attachable_type
+      t.timestamp
+    end
+  end
+end
