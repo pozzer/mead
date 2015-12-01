03ac19af753eff0a18693778b4d892f8e22ae77b
pozzer <feliphepozzer@gmail.com>
2015-11-30 22:47:58 -0200

    strong params no omniauth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,17 +1,16 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+
   def facebook
-
-     # with this code you can see the data sent by facebook
-     omniauth = request.env["omniauth.auth"]
-
-    @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
-
-    if @user.persisted?
+    if User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user).persisted?
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
-      sign_in_and_redirect @user, :event => :authentication
     else
-      session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
+      @user = User.from_omniauth(auth)
     end
-   end
+    sign_in_and_redirect @user, :event => :authentication
+  end
+
+  def auth
+    ActionController::Parameters.new(request.env["omniauth.auth"]).permit(:provider, :uid, :info => [:email, :name, :first_name, :last_name, :image, :verified, :urls=> [:Facebook] ], :credentials => [:token, :expires_at, :expires], :extra => [:raw_info => [:id, :email, :first_name, :gender, :last_name, :link, :locale, :name, :timezone, :updated_time, :verified ]] )
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -53,4 +53,16 @@ class User < ActiveRecord::Base
     end
   end
 
+  def self.from_omniauth(auth)
+    binding.pry
+    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
+      user.provider = auth[:provider]
+      user.uid = auth[:uid]
+      user.email = auth[:info][:email]
+      #user.oauth_token = auth[:credentials][:token]
+      #user.oauth_expires_at = Time.at(auth[:credentials][:expires_at])
+      user.save!
+    end
+  end
+
 end
