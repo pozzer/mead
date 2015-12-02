81493fbbeba3d995844ec88b973751aa88d9b8c2
pozzer <feliphepozzer@gmail.com>
2015-12-02 13:28:30 -0200

    alterações na logica do omniauth, adicionando o build profile no registration new

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,13 @@
 class RegistrationsController < Devise::RegistrationsController
 
+  def new
+    build_resource({})
+    resource.build_profile({ first_name: session["devise.facebook_data"]["info"]["first_name"] }) unless session["devise.facebook_data"].blank?
+    set_minimum_password_length
+    yield resource if block_given?
+    respond_with self.resource
+  end
+
   private
 
   def sign_up_params
@@ -8,7 +16,6 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def account_update_params
-  	binding.pry
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password, :profile => [:first_name, :birth_date])
+    params.require(:user).permit(:email, :password, :password_confirmation, :current_password, :profiles_attributes => [:first_name, :birth_date ]])
   end
 end
