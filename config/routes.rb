77a30ad0da5f1fd8670db2aee687ccf45f10939e
pozzer <feliphepozzer@gmail.com>
2015-12-13 13:40:04 -0200

    alteração de layout do application

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :bottles
   resources :questions
   resources :profiles
+  resources :homepages, only: :index
 
   devise_for :users,  :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", :registrations => "registrations" }
 
