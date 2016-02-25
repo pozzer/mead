6acadc88920200fa41f64f6a396880bba9c3cc7d
Feliphe Pozzer <feliphepozzer@gmail.com>
2016-02-25 09:16:44 -0300

    criandos os metodos necessários para a criação do trade

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -1,58 +1,33 @@
 class TradesController < ApplicationController
   before_action :set_trade, only: [:show, :edit, :update, :destroy]
+  before_action :mount_trade_params, only: :create
 
-  # GET /trades
-  # GET /trades.json
   def index
     @trades = Trade.all
   end
 
-  # GET /trades/1
-  # GET /trades/1.json
   def show
   end
 
-  # GET /trades/new
   def new
     @trade = Trade.new
   end
 
-  # GET /trades/1/edit
   def edit
   end
 
-  # POST /trades
-  # POST /trades.json
   def create
+    binding.pry
     @trade = Trade.new(trade_params)
-
-    respond_to do |format|
-      if @trade.save
-        format.html { redirect_to @trade, notice: 'Trade was successfully created.' }
-        format.json { render :show, status: :created, location: @trade }
-      else
-        format.html { render :new }
-        format.json { render json: @trade.errors, status: :unprocessable_entity }
-      end
-    end
+    @trade.save
+    respond_with(@btrade, :location => trades_path(@trade))
   end
 
-  # PATCH/PUT /trades/1
-  # PATCH/PUT /trades/1.json
   def update
-    respond_to do |format|
-      if @trade.update(trade_params)
-        format.html { redirect_to @trade, notice: 'Trade was successfully updated.' }
-        format.json { render :show, status: :ok, location: @trade }
-      else
-        format.html { render :edit }
-        format.json { render json: @trade.errors, status: :unprocessable_entity }
-      end
-    end
+    @trade.update_attributes(bottle_params)
+    respond_with(@btrade, :location => trades_path(@trade))
   end
 
-  # DELETE /trades/1
-  # DELETE /trades/1.json
   def destroy
     @trade.destroy
     respond_to do |format|
@@ -62,13 +37,20 @@ class TradesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_trade
       @trade = Trade.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+    def mount_trade_params
+      get_bottle
+      params[:trade] = {negotiant_id: @bottle.user_id, negotiator_id: current_user.id}
+    end
+
     def trade_params
-      params[:trade]
+      params[:trade].permit(:negotiant_id, :negotiator_id)
+    end
+
+    def get_bottle
+      @bottle = Bottle.find(params["bottle_id"].first)
     end
 end
