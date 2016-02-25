a98766fe6240751a92f7b1b7c96877468bd21411
Feliphe Pozzer <feliphepozzer@gmail.com>
2016-02-25 09:16:01 -0300

    adicionando as validações

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -1,12 +1,13 @@
 class Trade < ActiveRecord::Base
 	has_many :messages, class_name: "MessageTrade"
-	belongs_to :negotiator, class_name: "User"
+	has_many :bottle_trades
+  belongs_to :negotiator, class_name: "User"
 	belongs_to :negotiant, class_name: "User"
-	
-	STATUS = {
-		1 => "pendente",
-		2 => "pendente",
-		3 => "pendente"
-	}
+
+  validates_presence_of :negotiator, :negotiant
+
+  accepts_nested_attributes_for :bottle_trades, :allow_destroy => true
+
+  enum status: {general: 0, post_received: 1, put_received: 2, post_sended: 3, put_sended: 4}
 
 end
