3083b6a1ab3c20e5c8b9cfabf3be4041fc9348f1
pozzer <feliphepozzer@gmail.com>
2016-03-15 20:43:37 -0300

    adicionando os terms como parametro seguro

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,6 +3,6 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(resource_name).permit(:email, :password, :password_confirmation, :current_password, :profile_attributes => [:first_name, :birth_date ])
+    params.require(resource_name).permit(:email, :password, :password_confirmation, :current_password, :terms, :profile_attributes => [:first_name, :birth_date ])
   end
 end
