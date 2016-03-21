f9cb80764711671ab15ae3190fc8eae353db13b0
pozzer <feliphepozzer@gmail.com>
2016-03-20 23:06:52 -0300

    removendo o l perdido

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -168,7 +168,7 @@ Devise.setup do |config|
 
   # Defines which strategy will be used to unlock an account.
   # :email = Sends an unlock link to the user emai
-l  # :time  = Re-enables login after a certain amount of time (see :unlock_in below)
+  # :time  = Re-enables login after a certain amount of time (see :unlock_in below)
   # :both  = Enables both strategies
   # :none  = No unlock strategy. You should handle unlocking by yourself.
   config.unlock_strategy = :time
