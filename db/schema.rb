18effc3e28b95a50b8b5139c790e6467d3d7865b
pozzer <feliphepozzer@gmail.com>
2016-03-23 21:26:05 -0300

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20160321154927) do
-=======
 ActiveRecord::Schema.define(version: 20160322153103) do
->>>>>>> feature/admin
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-<<<<<<< HEAD
-=======
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
     t.text     "body"
@@ -37,7 +31,6 @@ ActiveRecord::Schema.define(version: 20160322153103) do
   add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace", using: :btree
   add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id", using: :btree
 
->>>>>>> feature/admin
   create_table "addresses", force: :cascade do |t|
     t.string   "postal_code"
     t.string   "state"
@@ -53,8 +46,6 @@ ActiveRecord::Schema.define(version: 20160322153103) do
 
   add_index "addresses", ["profile_id"], name: "index_addresses_on_profile_id", using: :btree
 
-<<<<<<< HEAD
-=======
   create_table "admin_users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -73,7 +64,6 @@ ActiveRecord::Schema.define(version: 20160322153103) do
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
 
->>>>>>> feature/admin
   create_table "answers", force: :cascade do |t|
     t.text     "content"
     t.integer  "user_id"
@@ -82,6 +72,10 @@ ActiveRecord::Schema.define(version: 20160322153103) do
     t.boolean  "best",        default: false, null: false
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
+  end
+
+  create_table "artigos", force: :cascade do |t|
+    t.string "title"
   end
 
   create_table "authorizations", force: :cascade do |t|
@@ -229,12 +223,12 @@ ActiveRecord::Schema.define(version: 20160322153103) do
     t.string   "picture_file_name"
     t.string   "picture_content_type"
     t.integer  "picture_file_size"
-    t.datetime "picture_updated_at"
+    t.datetime "picture_update_at"
     t.integer  "attachable_id"
     t.string   "attachable_type"
-    t.integer  "picture_type"
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
+    t.integer  "picture_type"
   end
 
   create_table "profiles", force: :cascade do |t|
