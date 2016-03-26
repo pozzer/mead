6282d262e3831644f9e50384f5d178a06629e1ca
pozzer <feliphepozzer@gmail.com>
2016-03-25 21:47:01 -0300

    fix consulta

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -20,9 +20,9 @@ class Profile < ActiveRecord::Base
   validates :first_name, presence: true
   validates :birth_date, presence: true, major_age: true
 
-  scope :joins_search, -> { "LEFT JOIN addresss ON addresses.profile_id = profiles.id 
+  scope :joins_search, -> { joins("LEFT JOIN addresses ON addresses.profile_id = profiles.id 
                              LEFT JOIN cities ON addresses.city_id = cities.id
-                             LEFT JOIN addresss ON addresses.states_id = state.id" }
+                             LEFT JOIN states ON addresses.state_id = states.id") }
 
   def full_name
     "#{first_name} #{last_name}"
