be07dd63469d9e2b2686be3eb8e47693d87cbfa7
Feliphe Pozzer <feliphepozzer@gmail.com>
2016-04-05 12:58:51 -0300

    removendo o host name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
     :storage => :s3,
-    :s3_host_name => 'REMOVE_THIS_LINE_IF_UNNECESSARY',
+    #:s3_host_name => 'REMOVE_THIS_LINE_IF_UNNECESSARY',
     :bucket => 'mead-production'
   }
 
