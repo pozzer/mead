02618dc382c58cdaded24c28e37d4712cac8a2ef
Feliphe Pozzer <feliphepozzer@gmail.com>
2016-05-30 11:21:59 -0300

    more fixes

--- app/assets/javascripts/bottles.js
+++ app/assets/javascripts/bottles.js
@@ -1,16 +1,16 @@
 if (window.mead_project === undefined) window.mead_project = {};
 
 mead_project.bottles = {
-    common: function() {
-      var priv = {};
-      var pub = {};
+  common: function() {
+    var priv = {};
+    var pub = {};
 
-      pub.init = function() {
-        return pub;
-      }
 
-    }()
+    pub.init = function() {
+    }
+    return pub;
+  }()
 };
-mead_project.bottles.index = mead_project.bottles.update =
-    mead_project.bottles.create = mead_project.bottles.new = mead_project.bottles.edit = mead_project.bottles.common;
+mead_project.bottles.index = mead_project.bottles.show = mead_project.bottles.update =
+    mead_project.bottles.create = mead_project.bottles.new = mead_project.bottles.edit = mead_project.bottles.show =  mead_project.bottles.common;
 
--- app/assets/javascripts/dashboards.js
+++ app/assets/javascripts/dashboards.js
@@ -1,15 +1,16 @@
 if (window.mead_project === undefined) window.mead_project = {};
 
 mead_project.dashboards = {
-    common: function() {
-      var priv = {};
-      var pub = {};
+  common: function() {
+    var priv = {};
+    var pub = {};
 
-      pub.init = function() {
-        return pub;
-      }
 
-    }()
+    pub.init = function() {
+    }
+    return pub;
+  }()
 };
-mead_project.dashboards.index = mead_project.dashboards.common;
+mead_project.dashboards.index = mead_project.dashboards.show = mead_project.dashboards.update =
+    mead_project.dashboards.create = mead_project.dashboards.new = mead_project.dashboards.edit = mead_project.dashboards.show =  mead_project.dashboards.common;
 
