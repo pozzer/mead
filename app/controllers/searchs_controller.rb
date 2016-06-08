36478e262118c3be2b82e1170df2a86a9b5368dd
pozzer <feliphepozzer@gmail.com>
2016-06-08 13:06:21 -0300

    fixes no search

--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -4,6 +4,8 @@ class SearchsController < ApplicationController
 		@questions = MonsterSearch.new(Question, params[:search]).search.page(params[:question_page])
 		@profiles = MonsterSearch.new(Profile, params[:search]).search.page(params[:profile_page])
 		@bottles = MonsterSearch.new(Bottle, params[:search]).search.page(params[:bottle_page])
+
+		@order_find = [@questions,@profiles,@bottles].sort_by{|e| -e.size }#.delete_if { |elem| elem.flatten.empty? }
 	end
 
 end
--- app/models/concept_question.rb
+++ app/models/concept_question.rb
@@ -7,8 +7,8 @@ class ConceptQuestion < ActiveRecord::Base
 	def self.create_tree(concepts, question)
 		concepts.each_with_index do |concept, index|
 			ConceptQuestion.create({
-				question: question
-				concept: concept
+				question: question,
+				concept: concept,
 				parent_concept: (index > 0 ? concepts[index-1] : nil)
 			})
 		end
